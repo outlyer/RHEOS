acaf3b81a3aa866dd1c40fafb8a5d74b30bdedba
Peter Richardson <petercrichardson@yahoo.com>
2024-10-01 09:06:41 -0400

    0.10.0-6

--- app.mjs
+++ app.mjs
@@ -1,4 +1,4 @@
-const version = "0.10.0-5"
+const version = "0.10.0-6"
 "use-strict"
 import RoonApi from "node-roon-api"
 import RoonApiSettings from "node-roon-api-settings"
@@ -1469,7 +1469,7 @@ async function connect_roon() {
 	const roon = new RoonApi({
 		extension_id: "com.RHEOS.latest",
 		display_name: "Rheos",
-		display_version: "0.10.0-5",
+		display_version: "0.10.0-6",
 		publisher: "RHEOS",
 		email: "rheos.control@gmail.com",
 		website: "https:/github.com/LINVALE/RHEOS",
